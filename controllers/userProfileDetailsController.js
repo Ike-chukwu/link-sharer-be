793cc87dfc38389b880f2d786eb20aa5af8f6f52
Ike-chukwu <103510036+Ike-chukwu@users.noreply.github.com>
2024-07-04 18:57:51 +0100

    fix:accounted for empty file or email inputs in form

--- controllers/userProfileDetailsController.js
+++ controllers/userProfileDetailsController.js
@@ -12,9 +12,19 @@ const addPersonalDetailsHandler = async (req, res) => {
   const actualUserDetail = await UserDetail.findOne({ userId: actualUser._id });
   actualUserDetail.firstName = req.body.firstName;
   actualUserDetail.lastName = req.body.lastName;
-  actualUserDetail.email = req.body.email;
-  actualUserDetail.imgUrl = req.body.imgUrl;
-  actualUserDetail.selectedFile = req.file.filename;
+  if (!req.file?.filename) {
+    actualUserDetail.imgUrl = "";
+    actualUserDetail.selectedFile = "";
+  } else {
+    actualUserDetail.selectedFile = req.file.filename;
+    actualUserDetail.imgUrl = req.body.imgUrl;
+  }
+  // console.log(req.body.imgUrl);
+  if (!req.body.email) {
+    actualUserDetail.email = "";
+  } else {
+    actualUserDetail.email = req.body.email;
+  }
   const updatedUserPersonalDetails = await actualUserDetail.save();
   res.status(201).json({
     message: "Personal Details successfully added",
